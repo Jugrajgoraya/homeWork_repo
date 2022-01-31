630edb1f9a42f32ab63658783ce2e985b49b6fa5
jugraj <jugraj98123@gmail.com>
2022-01-30 17:54:04 -0800

    added drawMiddleBorder fn

--- boxit.js
+++ boxit.js
@@ -22,3 +22,13 @@ function drawTopborder (num){
 console.log(drawTopborder(4))
 
 
+function drawMiddleBorder (num){
+    let lines = ""
+    for(let i =0; i<num; i++){
+        lines += "━";
+    }
+    lines ="┣" + lines + "┫"
+    return lines;
+}
+
+console.log(drawMiddleBorder(4))
