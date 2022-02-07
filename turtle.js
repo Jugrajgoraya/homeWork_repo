8edd1fc7c86951fa38aeaa29baf544f048f96c85
jugraj <jugraj98123@gmail.com>
2022-02-07 08:55:36 -0800

    added allpoints method plus fixed the forword method

--- turtle.js
+++ turtle.js
@@ -1,27 +1,41 @@
 class Turtle{
     constructor(x=0,y=0){
-        this.path = [(x,y)]
+        this.path = [[x,y]]
         this.angle = 0;
 
     }
-    forword(step){
-        if ((this.angle = 0) || (this.angle = 360)){
-            this.path.push((step,0))
-        } else if ((this.angle = 90) || (this.angle = -270)) {
-            this.path.push((0, step))
-        } else if ((this.angle = 180) || (this.angle = -180)) {
-            this.path.push((-step,0))
-        } else if ((this.angle = -90) || (this.angle = 270)) {
-            this.path.push((0,-step))
+    forward(step){
+        let arr = this.path[this.path.length-1]
+        if ((this.angle == 0) || (this.angle == 360)){
+            this.path.push([arr[0]+step,arr[1]])
+        } else if ((this.angle == 90) || (this.angle == -270)) {
+            this.path.push([arr[0], arr[1]+step])
+        } else if ((this.angle == 180) || (this.angle == -180)) {
+            this.path.push([arr[0]-step,arr[1]])
+        } else if ((this.angle == -90) || (this.angle == 270)) {
+            this.path.push([arr[0],arr[1]-step])
         }
+        return this
     }
     right(){
-        this.angle -=90;
+        this.angle  -= 90;
+        return this
     }
     
     left(){
         this.angle +=90;
+        return this
+    }
+
+    allPoints(){
+        this.path.forEach(element =>{
+        console.log(element)
+        })
     }
 
 }
 
+const t1 = new Turtle();
+t1.forward(3).right().forward(4).allPoints()
+const flash = new Turtle(0, 0).forward(3).left().forward(3).left().forward(10);
+flash.allPoints()
